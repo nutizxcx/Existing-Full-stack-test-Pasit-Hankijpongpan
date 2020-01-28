22fd79e0551efb402d749f405e2855f2a8595ca1
nutizxcx <nutzxcx@hotmail.com>
2020-01-28 23:29:05 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ListHotelComponent } from './list-hotel/list-hotel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ListHotelComponent
   ],
   imports: [
     BrowserModule,
